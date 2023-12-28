50468c4230bf7ec87c76a7e088d50f61ddc7691e
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-28 18:27:45 +0900

    add

--- index.ts
+++ index.ts
@@ -3,15 +3,15 @@ import { Webview } from "https://deno.land/x/webview@0.7.6/mod.ts";
 export class App {
   webview: Webview | null = null;
 
-  constructor(public url: string, public title: string, options?: any[]) {
+  constructor(public url: string, public title: string, options?: boolean[]) {
     this.webview = new Webview(...(options || []));
 
     this.webview.navigate(this.url);
     this.webview.title = this.title;
   }
 
-  run(middleware: Function) {
+  run(middleware: (webview: Webview, self: App) => void) {
     this.webview!.run();
-    middleware(this.webview, this);
+    middleware(this.webview!, this);
   }
 }
